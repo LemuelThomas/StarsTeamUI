78c0b47a01c4bcc15208437e1f4b926360a6c4a4
treymeetsworld <trevorzac16@yahoo.com>
2022-06-23 13:36:31 -0400

    added tv show details component

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,43 +6,43 @@ function Login() {
     return (
         <div className="login">
             <div className="login_background">
-                <img 
-                className="login_logo"
-                src="https://images.indianexpress.com/2021/04/pixabay_shooting-star_1200.jpg" 
-                alt="" 
+                <img
+                    className="login_logo"
+                    src="https://images.indianexpress.com/2021/04/pixabay_shooting-star_1200.jpg"
+                    alt=""
                 />
-        <button onClick={() => setSignIn(true)}
-         className="login_button">
-        Sign In
-        </button>
-        <div className="login_gradient" />
-        
-        </div>
-        <div className="login_body">
-        <>
-        <h1> Stream movies and tv on demand
-        </h1>
-       
-        <h2>Free Sign-Up.</h2>
-        
-        <h3>Ready to stream? Enter your email to start your account</h3>
+                <button onClick={() => setSignIn(true)}
+                    className="login_button">
+                    Sign In
+                </button>
+                <div className="login_gradient" />
 
-        <div className="login_input"></div>
-        <form> 
-            <input type='email'
-            placeholder='Email Address'
-            />
-             <input type='password'
-            placeholder='Password'
-            />
-            <button 
-            onClick={() => setSignIn(true )}
-            className='login_getStarted'>Get Started</button>
+            </div>
+            <div className="login_body">
+                <>
+                    <h1> Stream movies and tv on demand
+                    </h1>
 
-        </form>
-        </>
+                    <h2>Free Sign-Up.</h2>
+
+                    <h3>Ready to stream? Enter your email to start your account</h3>
+
+                    <div className="login_input"></div>
+                    <form>
+                        <input type='email'
+                            placeholder='Email Address'
+                        />
+                        <input type='password'
+                            placeholder='Password'
+                        />
+                        <button
+                            onClick={() => setSignIn(true)}
+                            className='login_getStarted'>Get Started</button>
+
+                    </form>
+                </>
+            </div>
         </div>
-</div>
     )
 }
 
---
+++ src/components/TvDetails.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Link, useParams } from 'react-router-dom'
+
+
+type Props = {
+  id: number;
+  title: string;
+  name: string;
+  poster_path: string;
+  backdrop_path: string;
+  overview: string;
+  videos: [];
+}
+
+
+export default function TvDetails() {
+  
+    const {id} = useParams()
+      
+
+      const [tvData, setTvData] = useState<Props | null>()
+      const [keyData, setKeyData] = useState<string>('')
+      
+  useEffect(() =>{
+
+    axios.get( `https://api.themoviedb.org/3/tv/${id}?api_key=${process.env.REACT_APP_API_KEY}&append_to_response=videos`)
+    .then(res => {
+      setTvData(res.data)
+      setKeyData(res.data.videos.results[0].key)
+      
+    })
+  }, []) 
+  
+  return (
+    <>
+    <Link to={'/'}>Homepage</Link>
+      <h2>TV Show Details</h2>
+      <div className="details_container">
+        <h3>{tvData?.name}</h3>
+          <div className="details_box">
+            <img src={`https://image.tmdb.org/t/p/w400${tvData?.poster_path
+                  }`}alt="image" />
+          <iframe  width="850" height="647"
+            src={`https://www.youtube.com/embed/${keyData}?autoplay=1`}
+            id='player'>
+          </iframe>
+        </div>
+          <h4>{tvData?.overview}</h4>
+      </div>
+    </>
+  )
+}
