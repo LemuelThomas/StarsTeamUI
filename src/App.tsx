dc4f5329cef15cd77b77ac114a59a9f80fc4f0f7
Gerard E. Paul <gerardpaul@outlook.com>
2022-06-30 13:26:50 -0500

    Deleted the static function that was being called

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
 import "./App.css";
 import Display_FAQs from "./components/Display_FAQs";
-import React  from 'react';
-import Homepage from './components/Homepage';
-import GetMovie from './components/GetMovie';
-import { Route, Routes } from 'react-router-dom'
-import ProfileScreen from './components/ProfileScreen';
-import Register from './components/Register';
-import Login from './components/Login';
-import MovieDetails from './components/MovieDetails';
-import TvDetails from './components/TvDetails';
-import Settings from './components/Settings';
-import Shows from './components/Shows';
+import React from "react";
+import Homepage from "./components/Homepage";
+import GetMovie from "./components/GetMovie";
+import { Route, Routes } from "react-router-dom";
+import ProfileScreen from "./components/ProfileScreen";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import MovieDetails from "./components/MovieDetails";
+import TvDetails from "./components/TvDetails";
+import Settings from "./components/Settings";
+import Shows from "./components/Shows";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/profile' element={<ProfileScreen />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/profile" element={<ProfileScreen />} />
         {/* <Route path='/profile/:id' element={<ProfileScreen />} /> */}
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/settings' element={<Settings/>} />
-        <Route path='/settings/:id' element={<Settings/>} />
-        <Route path='/movies' element={<GetMovie />} />
-        <Route path='/movies/:id' element={<MovieDetails />} />
-        <Route path='/shows/:id' element={<TvDetails />} />
-        <Route path='/shows' element={<Shows />} />
-        <Route path='/faqs' element={<Display_FAQs />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/settings/:id" element={<Settings />} />
+        <Route path="/movies" element={<GetMovie />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/shows/:id" element={<TvDetails />} />
+        <Route path="/shows" element={<Shows />} />
+        <Route path="/faqs" element={<Display_FAQs />} />
       </Routes>
     </>
   );
